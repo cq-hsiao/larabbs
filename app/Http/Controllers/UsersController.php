0ea9a4aec7018fe840ec86697dc70e897f6efb84
cq-hsiao <124328227@qq.com>
2019-08-22 09:43:23 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -38,10 +39,18 @@ class UsersController extends Controller
 //        return redirect()->route('users.show',$user->id)->with('success', '个人资料更新成功！');
 //    }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader)
     {
 
-        $user->update($request->all());
+//        dd($request->avatar);
+        $data = $request->all();
+        if($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success', '个人资料更新成功！');
     }
 
