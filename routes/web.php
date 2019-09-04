62df881ad469f7c2da7a6c540442bee1d45d424d
“cq-hsiao” <124328227@qq.com>
2019-09-04 09:52:41 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,6 @@ Route::resource('users','UsersController',['only'=>['show','edit','update']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories','CategoriesController',['only'=>'show']);
+
+//上传图片
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
