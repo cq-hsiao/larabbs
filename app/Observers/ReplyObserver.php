d620a0523d38eee1b46587e6b371a71fc7ae171c
“cq-hsiao” <124328227@qq.com>
2019-09-07 03:35:19 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,22 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        //        dd($reply->topic()->firstOrFail());
+        //        dd($reply->topic);
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
-    public function updating(Reply $reply)
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function saving(Reply $reply)
+    {
+        $reply->content = clean($reply->content, 'user_topic_body');
+        if(!$reply->content) return false;
     }
 }
