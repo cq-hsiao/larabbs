227025d6d578c70390b15905b0077ea145431104
“cq-hsiao” <124328227@qq.com>
2020-01-09 10:00:13 +0000

    话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\TopicsRequest;
-use App\Http\Resources\TopicsResource;
+use App\Http\Resources\TopicResource;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class TopicsController extends Controller
 {
@@ -17,7 +20,7 @@ class TopicsController extends Controller
         $topic->user_id = $request->user()->id;
         $topic->save();
 
-        return new TopicsResource($topic);
+        return new TopicResource($topic);
     }
 
 
@@ -26,7 +29,7 @@ class TopicsController extends Controller
         $this->authorize('update',$topic);
 
         $topic->update($request->all());
-        return new TopicsResource($topic);
+        return new TopicResource($topic);
     }
 
     public function destroy(Topic $topic)
@@ -36,5 +39,74 @@ class TopicsController extends Controller
         $topic->delete();
 
         return response(null, 204);
+    }
+
+
+    public function index(Request $request,Topic $topic){
+
+//        $query = $topic->query();
+//
+//        if($categoryId = $request->category_id)
+//        {
+//            $query->where('category_id',$categoryId);
+//        }
+//
+//        $topics = $query->withOrder($request->order)->paginate();
+
+        // allowedIncludes 方法传入可以被 include 的参数 /topics?include=user,category —— 返回话题数据、发布者的数据，以及所属的分类数据。
+        /* allowedFilters 方法传入可以被搜索的条件，可以传入某个字段，例如我们这里传入了 title，这样会模糊搜索标题；
+        如果某个字段是精确搜索需要进行指定，这里我们指定 category_id 是精确搜索的；
+        还可以传入某个 scope，并且制定默认的参数，例如这里我们指定可以使用 withOrder 进行搜索，默认的值是 recentReplied。
+        使用 filter 参数可以进行搜索，该参数是个数组。
+        例如我们搜索标题，分类以及按 recent 排序。
+        */
+
+        $beginTime = microtime(true);
+
+        $topics = QueryBuilder::for(Topic::class)
+//            ->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])
+            ->paginate();
+
+        $include = !empty($request->include) ? explode(',',$request->include) : [];
+
+
+        $topics->transform(function ($customer) use ($include) {
+//            unset($customer->user);
+//            $aa = $customer->setHidden(['user','category']);
+//            dd($aa);
+            $diff = array_diff(array_keys($customer->getRelations()),$include);
+            foreach ($diff as $item){
+                $customer->unsetRelation($item);
+            }
+            return $customer;
+        });
+
+        $endTime = microtime(true);
+        \Log::info($endTime - $beginTime);
+
+
+        return TopicResource::collection($topics);
+    }
+
+
+    public function userIndex(Request $request, User $user)
+    {
+        $query = $user->topics()->getQuery();
+
+        $topics = QueryBuilder::for($query)
+            ->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])
+            ->paginate();
+
+        return TopicResource::collection($topics);
     }
 }
